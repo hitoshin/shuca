27161503966df8ef7b24474faab53e4031fd1ddc
NISHIKAWA Hitoshi <hitoshi@nishikawa.name>
2015-09-03 14:13:13 +0900

    Sentence splitter was improved.

--- libexec/SentenceSplitter.py
+++ libexec/SentenceSplitter.py
@@ -5,14 +5,30 @@ import re
 import sys
 
 indentation_pattern = re.compile(u'^　')
-reporter_pattern = re.compile(u'^【.+?】')
+reporter_pattern = re.compile(u'(?:^【.+?】|【.+?】$)')
 
-for line in sys.stdin:
-    line = unicode(line, 'utf-8')
-    line = line.rstrip()
-    array = line.split(u'。')
-    for sentence in array:
-        if sentence != '':
-            sentence = indentation_pattern.sub('', sentence)
-            sentence = reporter_pattern.sub('', sentence)
-            print '%s%s' % (sentence.encode('utf-8'), '。')
+body = unicode(sys.stdin.read().rstrip(), 'utf-8')
+buffer = ''
+sentences = []
+p_flag = False
+
+for i in range(0, len(body)):
+    c = body[i:i+1]
+    if c != u'\n':
+        buffer = buffer + c
+    if c == u'。' and p_flag == False:
+        sentences.append(buffer)
+        buffer = ''
+    if c == u'「':
+        p_flag = True
+    if c == u'」' and p_flag == True:
+        p_flag = False
+        if body[i+1:i+2] == u'\n':
+            sentences.append(buffer)
+            buffer = ''
+
+for sentence in sentences:
+    sentence = indentation_pattern.sub('', sentence)
+    sentence = reporter_pattern.sub('', sentence)
+    if sentence != '':
+        print '%s' % (sentence.encode('utf-8'))
